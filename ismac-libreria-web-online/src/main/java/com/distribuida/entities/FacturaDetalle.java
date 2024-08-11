a71ba097b732cca263c4f3dc607cae0f66af11cb
coxdi <coxdi@DCox>
2024-08-11 16:26:10 +0200

    DCv0008 Parte 6.1.  CRUD SIMPLE: Modulo WEB Cliente y Parte 6.3.  CRUD COMPUESTO: Modulo WEB Libro

---
+++ ismac-libreria-web-online/src/main/java/com/distribuida/entities/FacturaDetalle.java
@@ -0,0 +1,108 @@
+package com.distribuida.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name = "factura_detalle")
+public class FacturaDetalle {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_factura_detalle")
+	public int idFacturaDetalle;
+	@Column(name = "cantidad")
+	public int cantidad;
+	@Column(name = "subtotal")
+	public double subtotal;
+	
+	@JoinColumn(name = "id_factura")
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	public Factura factura;  //private int idFactura;
+	
+	@JoinColumn(name = "id_libro")
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+	public Libro libro;      //private int idLibro;
+	
+	public FacturaDetalle() {}
+	
+	
+	public FacturaDetalle(int idFacturaDetalle, int cantidad, double subtotal, Factura factura, Libro libro) {
+		
+		this.idFacturaDetalle = idFacturaDetalle;
+		this.cantidad = cantidad;
+		this.subtotal = subtotal;
+		this.factura = factura;
+		this.libro = libro;
+	}
+
+
+	public int getIdFacturaDetalle() {
+		return idFacturaDetalle;
+	}
+
+
+	public void setIdFacturaDetalle(int idFacturaDetalle) {
+		this.idFacturaDetalle = idFacturaDetalle;
+	}
+
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+
+	public Factura getFactura() {
+		return factura;
+	}
+
+
+	public void setFactura(Factura factura) {
+		this.factura = factura;
+	}
+
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FacturaDetalle [idFacturaDetalle=" + idFacturaDetalle + ", cantidad=" + cantidad + ", subtotal="
+				+ subtotal + ", factura=" + factura + ", libro=" + libro + "]";
+	}
+	
+
+	
+}
